9bf00ce7ddc46429898524f40f200680fae7e799
Roger Liu <rogliu26@gmail.com>
2016-03-15 20:31:04 -0400

    made a basic keyboard version with a backspace button.

---
+++ LetterHighlightSpellCheck/ngrams/dictSplitter.py
@@ -0,0 +1,19 @@
+def splitAndWrite(filename):
+    myFile = open(filename);
+    wordFreq = dict();
+
+    for speech in myFile :
+        word, count = speech.split("\t");
+        wordFreq[word] = count;
+        
+    wordFile = open("autoCompleteWords.txt", "w");
+    countFile = open("autoCompleteCounts.txt", "w");
+
+    for word in wordFreq.keys():
+        wordFile.write(word + "\n");
+        countFile.write(wordFreq[word])
+
+    wordFile.close();
+    countFile.close();
+
+splitAndWrite("count_1w.txt");
